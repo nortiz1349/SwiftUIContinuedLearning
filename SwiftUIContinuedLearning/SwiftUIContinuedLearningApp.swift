0c67433e29418666ecbd0c3cfba2d2867f03312b
Nortiz <nortiz1349@gmail.com>
2022-10-13 09:17:35 +0900

    Initial Commit

---
+++ SwiftUIContinuedLearning/SwiftUIContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIContinuedLearningApp.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by Nortiz M1 on 2022/10/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
